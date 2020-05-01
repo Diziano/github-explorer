5e436a7d79e497dfc55ccbfa0ec0afe0d0cc3b84
Diziano <dizianomachado@gmail.com>
2020-05-01 17:04:21 -0300

    Implements modifications to global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
+import githubBackground from '../assets/github-background.svg';
+
 export default createGlobalStyle`
   * {
     margin: 0;
@@ -9,7 +11,7 @@ export default createGlobalStyle`
   }
 
   body {
-    background: #F0F0F5;
+    background: #F0F0F5 url(${githubBackground}) no-repeat 70% top;
     -webkit-font-smoothing: antialiased;
   }
 
@@ -18,6 +20,13 @@ export default createGlobalStyle`
   }
 
   #root {
+    max-width: 960px;
+    margin: 0 auto;
     padding: 40px 20px;
   }
+
+  button {
+    cursor: pointer;
+  }
+
 `;
