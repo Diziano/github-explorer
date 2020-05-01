be74eb2dd4f50d7e7c5c73ffdb0e2d8a09e07964
Diziano <dizianomachado@gmail.com>
2020-05-01 20:32:21 -0300

    Integration with Github APi and list of repositories

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,60 +1,66 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/948514?s=460&u=d5dcceb2fc93261cdde28cf7e67a32dc8af27436&v=4"
-            alt="Diziano"
-          />
-          <div>
-            <strong>diziano/gobarber-backend</strong>
-            <p>Back-end da aplicação desenvolvida no Bootcamp GoStack 11</p>
-          </div>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/948514?s=460&u=d5dcceb2fc93261cdde28cf7e67a32dc8af27436&v=4"
-            alt="Diziano"
-          />
-          <div>
-            <strong>diziano/gobarber-backend</strong>
-            <p>Back-end da aplicação desenvolvida no Bootcamp GoStack 11</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/948514?s=460&u=d5dcceb2fc93261cdde28cf7e67a32dc8af27436&v=4"
-            alt="Diziano"
-          />
-          <div>
-            <strong>diziano/gobarber-backend</strong>
-            <p>Back-end da aplicação desenvolvida no Bootcamp GoStack 11</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
