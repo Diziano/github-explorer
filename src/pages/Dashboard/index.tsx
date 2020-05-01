8ef5965f134a42d5ff12c95801d7af052279b73c
Diziano <dizianomachado@gmail.com>
2020-05-01 17:55:07 -0300

    Dashboard styles implemented

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/948514?s=460&u=d5dcceb2fc93261cdde28cf7e67a32dc8af27436&v=4"
+            alt="Diziano"
+          />
+          <div>
+            <strong>diziano/gobarber-backend</strong>
+            <p>Back-end da aplicação desenvolvida no Bootcamp GoStack 11</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/948514?s=460&u=d5dcceb2fc93261cdde28cf7e67a32dc8af27436&v=4"
+            alt="Diziano"
+          />
+          <div>
+            <strong>diziano/gobarber-backend</strong>
+            <p>Back-end da aplicação desenvolvida no Bootcamp GoStack 11</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/948514?s=460&u=d5dcceb2fc93261cdde28cf7e67a32dc8af27436&v=4"
+            alt="Diziano"
+          />
+          <div>
+            <strong>diziano/gobarber-backend</strong>
+            <p>Back-end da aplicação desenvolvida no Bootcamp GoStack 11</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
